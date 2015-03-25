0334fbd5f63437371cd058807bfc69ff33edc605
tyler goodman <tylercgoodman@gmail.com>
2015-03-25 13:07:45 -0700

    client/admin separation maybe

--- app/client/client.js
+++ app/client/client.js
@@ -1,48 +1,66 @@
-(function adminModule (angular) {
-'use strict'
+(function clientModule(angular) {
+  'use strict';
 
-var app = angular.module('tangeloAdmin',
-	['demoServices', 'tangeloNodeServices', 'tangeloUserServices',
-	 'userDirectives', 'contentDirectives', 'editorServices']);
+  var app = angular.module('tangelo', [
+    'ngRoute',
+    'tangeloNodeServices',
+    'tangeloUserServices',
+    'userDirectives',
+    'contentDirectives',
+    'editorServices',
+  ]);
 
-var current = '';
+  app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/home', {
+      templateUrl: 'app/client-partials/home.html',
+      controller: 'ClientHomeController'
+    })
+    .when('/profile', {
+      templateUrl: 'app/client-partials/profile.html',
+      controller: 'ClientHomeController'
+    })
+    .when('/scratch', {
+      templateUrl: 'app/client-partials/scratch.html',
+      controller: 'ClientHomeController'
+    })
+    .when('/lesson', {
+      templateUrl: 'app/client-partials/lesson.html',
+      controller: 'ClientHomeController'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+  }]);
 
-app.controller('ClientMainController', ['$scope','$location','stratchService', function($scope, $location, stratchService){
+  app.controller('ClientMainController', ['$scope', '$location', 'stratchService', function ($scope, $location, stratchService) {
+    var current = '';
 
-  $scope.showPanel = false;
-  $scope.scratchText = {value:''};
-  $scope.panelChange = function (e) {
-  	stratchService.set($scope.scratchText.value);
-  	var href = e.target.getAttribute('link');
-  	if (href == current) {
-  		$scope.showPanel = false;
-  		current = '';
- 		return;
-  	} else {  		
-  		current = href;
-  	}
+    $scope.showPanel = false;
+    $scope.scratchText = {
+      value: ''
+    };
+    $scope.panelChange = function (e) {
+      stratchService.set($scope.scratchText.value);
+      var href = e.target.getAttribute('link');
+      if (href == current) {
+        $scope.showPanel = false;
+        current = '';
+        return;
+      }
+      else {
+        current = href;
+      }
 
 
-  	$location.path(href);
-	$scope.showPanel = true;
-  };
-	
-}]);
+      $location.path(href);
+      $scope.showPanel = true;
+    };
+  }]);
 
-app.controller('ClientHomeController', ['$scope','globalCounterService',function($scope, globalCounterService){
-  $scope.counter = 0;
-  $scope.globalCounter = globalCounterService.get();
-
-  $scope.increment = function counterInc() {
-      globalCounterService.inc();
-      $scope.globalCounter = globalCounterService.get();
-  };
-
-}]);
-
-app.controller('AdminUserController', ['$scope', 'userService', function($scope, userService){
-	$scope.users = userService;
-}])
+  app.controller('AdminUserController', ['$scope', 'userService', function ($scope, userService) {
+    $scope.users = userService;
+  }])
 
 
 })(angular);
